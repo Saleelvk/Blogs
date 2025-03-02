9df3dbc3000c9b6af3823a3d1bc4c1c8e9935305
Saleel vk <87797952+Saleelvk@users.noreply.github.com>
2025-03-02 15:08:24 +0530

    Update api.js

--- Frontend/src/Components/api.js
+++ Frontend/src/Components/api.js
@@ -1,12 +1,8 @@
-
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://blogspace-jh9a.onrender.com/api/v1",
 });
-
-
-
 const imgUrl = "https://blogspace-jh9a.onrender.com";
 
 export { api, imgUrl }; 
